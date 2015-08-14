ca3a0d5dd15f7ae15a413343a83e43a5c0a32b4d
Nicholas Chee-Awai <cheeordie1@gmail.com>
2015-08-13 23:20:04 -0500

    full chat working. Start on accounts

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,7 @@ module Doge
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.users = 0
+    config.tab_id = 0
   end
 end
