78a911315c336235e0762e980db006d7ff360190
Nicholas Chee-Awai <cheeordie1@gmail.com>
2015-08-23 16:41:48 -0500

    Added random doge title. Much Hax, So wow!

--- app/assets/javascripts/accounts/login_form.js
+++ app/assets/javascripts/accounts/login_form.js
@@ -2,6 +2,18 @@ $(document).ready (function ()
   {
     $("#exit_button").on ("mouseover", function ()
       {
-        alert("ow!");
+        $("#exit_button").attr ("src", "assets/exit_highlight.png");
       });
+    $("#exit_button").on ("mouseout", function ()
+      {
+        $("#exit_button").attr ("src", "assets/exit_normal.png");
+      });
+    $("#exit_button").on ("mousedown", function ()
+      {
+        $("#exit_button").attr ("src", "assets/exit_pressed.png");
+      });
+    $("#exit_button").on ("click", function ()
+      {
+        $("#login_form").remove ();
+      });    
   });
--- app/assets/javascripts/application/login.js
+++ app/assets/javascripts/application/login.js
@@ -1,12 +1,82 @@
+var title_first = ["So", "Such", "Much", "How", "Many", "Very", "Moar"];
+var title_second = ["Wow!", "Wow.", "Doge", "Cute!", "Amaze!", "Soft", "Cuddle", "Excite!", "Cool", "Happi", "Euphoric"];
+var title_length;
+var random_title = function ()
+  {
+    // 25/75 chance 0 or 1
+    title_length = Math.random ();
+    if (title_length <= 0.25)
+      return title_second [Math.floor (Math.random () * title_second.length)];
+    else
+      return title_first [Math.floor (Math.random () * title_first.length)] + 
+	     " " + 
+	     title_second [Math.floor (Math.random () * title_second.length)];
+  };
+
+var random_color = function ()
+  {
+    return "#" + Math.floor (Math.random () * 0xFFFFFF).toString (16);
+  };
+
+var random_shadow = function ()
+  {
+    var h_shadow, v_shadow, blur_radius;
+    if (Math.random () >= 0.3)
+      return "";
+    else
+      {
+	h_shadow = Math.floor (Math.random () * 5) - 2;
+        v_shadow = Math.floor (Math.random () * 5) - 2;
+        blur_radius = Math.floor (Math.random () * 3) + 6;
+	return h_shadow.toString () + "px " + v_shadow.toString () + "px " +
+	       blur_radius.toString () + "px " + random_color ();
+      }
+  };
+
+var font_families = ["Oswald", "Slabo", "Indie+Flower", "Dosis", "Yanone+Kaffeesatz",
+                     "Playfair+Display", "Cabin", "Shadows+Into+Light", "Sigmar+One", 
+		     "Orbitron", "Covered+By+Your+Grace", "Bangers"];
+var WebFontConfig, random_font_family;
+
+function load_random_font ()
+  {
+    random_font_family = font_families [Math.floor (Math.random () * font_families.length)];
+    WebFontConfig = 
+    {
+      google:
+        { 
+          families: [ random_font_family + "::latin" ]
+	}
+    };
+    $.get (('https:' == document.location.protocol ? 'https' : 'http') +
+            '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js',
+      function (data) { return true });
+  };
+
+load_random_font ();
+
 $(document).ready (function ()
   {
-    var login_form;
+    var login_form, font_family;
+
+    $("#doge_title").html (random_title ());
+    $("#doge_title").css ("color", random_color ());
+    font_family = "'" + random_font_family + "'";
+    font_family.replace ("+", " ");
+    $("#doge_title").css ("font-family", font_family);
+    $("#doge_title").css ("text-shadow", random_shadow ())
+
+    // Make Script transport through ajax asynchronous
+    $.ajaxPrefilter (function (options, originalOptions, jqXHR)
+      {
+        options.async = true;
+      });
 
     $("#login_link").on ("click", function ()
       {
 	if ($("#login_form").length)
 	  return;
-	$.get("/login", function (data)
+	$.get ("/login", function (data)
           {
 	    login_form = $(data).filter ("#login_form").clone (true);
             login_form.appendTo ("body");
