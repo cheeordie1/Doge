9a33caa4e89d1043537e4dfb5a7e35e61ae4bf74
Nicholas Chee-Awai <cheeordie1@gmail.com>
2015-08-28 08:48:32 -0500

    Added signup link. Needs beautify

--- app/assets/javascripts/application/login.js
+++ app/assets/javascripts/application/login.js
@@ -70,15 +70,57 @@ $(document).ready (function ()
         options.async = true;
       });
 
+    $("#signup_link").on ("ajax:beforeSend", function (event, xhr, settings)
+      {
+        if ($("#signup_form_container").length)
+          {  
+            xhr.abort ();
+	    return;
+	  }
+        $("#signup_tab").css ("border-color", "#333333");
+        $("#login_tab").css ("border-color", "#888888");
+      });
+
     $("#login_link").on ("ajax:beforeSend", function (event, xhr, settings)
       {
         if ($("#login_form_container").length)
-          xhr.abort ();   
+          {
+            xhr.abort ();
+	    return;
+	  }
+        $("#login_tab").css ("border-color", "#333333");
+        $("#signup_tab").css ("border-color", "#888888");
+      });
+   
+    $("#signup_tab").on ("click", function ()
+      {
+        $("#signup_link").click ();
+      });
+
+    $("#login_tab").on ("click", function ()
+      {
+        $("#login_link").click ();
+      });
+
+    $("#signup_link").on ("ajax:success", function (event, data, status, xhr)
+      {
+       	login_form = $(data).filter ("#signup_form_container").clone (true);
+	if ($("#login_form_container").length)
+          $("#login_form_container").remove ();
+	login_form.appendTo ("#center_form");
+	$("#signup_cover").css ("visibility", "visible");
+	$("#login_cover").css ("visibility", "hidden");
+        $("#ls_form_container").css ("display", "block");
       });
 
     $("#login_link").on ("ajax:success", function (event, data, status, xhr)
       {
        	login_form = $(data).filter ("#login_form_container").clone (true);
-        login_form.appendTo ("body");
+	if ($("#signup_form_container").length)
+          $("#signup_form_container").remove ();
+	login_form.appendTo ("#center_form");
+	$("#login_cover").css ("visibility", "visible");
+	$("#signup_cover").css ("visibility", "hidden");
+        $("#ls_form_container").css ("display", "block");
       });
   });
